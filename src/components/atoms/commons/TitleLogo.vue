2411d0f25715e8a7ec05b74aab72f456fdbb7b6e
hato-poppo <ysng1224@gmail.com>
2021-06-17 12:24:33 +0000

    [Change] Installation of atomic design.

---
+++ src/components/atoms/commons/TitleLogo.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-img
+    alt="Vuetify Logo"
+    class="shrink mr-2"
+    contain
+    src="@/assets/images/vuetify-logo-dark.png"
+    transition="scale-transition"
+    width="40"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({});
+</script>
---
+++ src/components/atoms/commons/TitleName.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-img
+    alt="Vuetify Name"
+    class="shrink mt-1 hidden-sm-and-down"
+    contain
+    min-width="100"
+    src="@/assets/images/vuetify-name-dark.png"
+    width="100"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({});
+</script>
