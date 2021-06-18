11ea7ae24aadd7ee96de9923945031bd9b03901e
hato-poppo <ysng1224@gmail.com>
2021-06-18 13:21:13 +0000

    [Change] To atomic design.

---
+++ src/components/atoms/list-items/Avatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-list-item-avatar>
+    <v-img :src="src" />
+  </v-list-item-avatar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    src: String,
+  },
+});
+</script>
---
+++ src/components/atoms/list-items/Text.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-list-item-content>
+    <v-list-item-title>{{ content }}</v-list-item-title>
+  </v-list-item-content>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    content: String,
+  },
+});
+</script>
