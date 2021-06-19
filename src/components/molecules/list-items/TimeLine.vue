a9d5ca2d67514493dc4c323cbeb4cad768fb87c0
hato-poppo <ysng1224@gmail.com>
2021-06-19 14:03:32 +0000

    [Add] Sidebar components.

---
+++ src/components/molecules/list-items/TimeLine.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list-item link>
+    <v-list-item-icon>
+      <v-badge
+        color="red"
+        :content="badgeCount"
+        overlap
+      >
+        <v-icon>mdi-message-text</v-icon>
+      </v-badge>
+    </v-list-item-icon>
+    <v-list-item-title>Time Lines</v-list-item-title>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    badgeCount: Number,
+  },
+});
+</script>
