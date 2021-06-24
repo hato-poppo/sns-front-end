6da490f4ab95dcbb7a6ed603aaf0ac81f3ea2f89
hato-poppo <ysng1224@gmail.com>
2021-06-24 14:57:58 +0000

    [Add] Post component.

---
+++ src/components/organisms/contents/Post.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card
+    class="mx-auto"
+    color="#8CD790"
+    dark
+    max-width="800"
+  >
+    <v-card-title>
+      <v-icon
+        large
+        left
+      >
+        mdi-message-text
+      </v-icon>
+      <span class="title font-weight-light">title</span>
+    </v-card-title>
+
+    <v-card-text color="#77AF9C" class="headline font-weight-bold" style="white-space: pre-line;">
+      text
+    </v-card-text>
+
+    <v-card-actions>
+      <!-- <v-list-item class="grow"></v-list-item> -->
+      <login-user
+        class="px-2 py-1"
+        @click="alert('toProfile')"
+        :avatar="require('@/assets/avatars/2220812101741.jpg')"
+        :name="'hato-poppo'"
+      />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import LoginUser from '@/components/molecules/list-items/LoginUser.vue';
+
+export default defineComponent({
+  components: {
+    'login-user': LoginUser,
+  },
+});
+</script>
--- src/views/TimeLine.vue
+++ src/views/TimeLine.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>Posts</h1>
+  <v-container>
+    <v-row>
+      <v-col>
+        <post />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
+import Post from '@/components/organisms/contents/Post.vue';
 
-export default defineComponent({});
+export default defineComponent({
+  components: {
+    Post,
+  },
+});
 </script>
