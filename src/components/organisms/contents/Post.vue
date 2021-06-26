b5c66a9dffcca0e3fd6c1022dd48e40d992cc11d
hato-poppo <ysng1224@gmail.com>
2021-06-26 14:21:44 +0000

    [Add] Width prop.

--- src/components/organisms/contents/Post.vue
+++ src/components/organisms/contents/Post.vue
@@ -3,7 +3,7 @@
     class="mx-auto"
     color="#8CD790"
     dark
-    max-width="800"
+    :max-width="width"
   >
     <v-card-title>
       <v-icon
@@ -20,10 +20,8 @@
     </v-card-text>
 
     <v-card-actions>
-      <!-- <v-list-item class="grow"></v-list-item> -->
       <login-user
         class="px-2 py-1"
-        @click="alert('toProfile')"
         :avatar="require('@/assets/avatars/2220812101741.jpg')"
         :name="value.user_name"
       />
@@ -41,6 +39,10 @@ export default defineComponent({
   },
   props: {
     value: Object,
+    width: {
+      type: Number,
+      default: 500,
+    },
   },
 });
 </script>
