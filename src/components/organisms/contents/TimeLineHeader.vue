9a89f5f22d70d814464b4af2b4522fb370e7926f
hato-poppo <ysng1224@gmail.com>
2021-06-28 12:58:51 +0000

    [Add] TimeLine page header.

---
+++ src/components/organisms/contents/TimeLineHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-toolbar
+    class="mx-auto"
+    :max-width="width"
+    elevation="1"
+  >
+    <v-btn color="primary" icon dark><v-icon>mdi-pencil-plus</v-icon></v-btn>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    width: {
+      type: Number,
+      default: 500,
+    },
+  },
+});
+</script>
