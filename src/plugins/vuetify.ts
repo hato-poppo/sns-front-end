25d87d76317c328f2f54f62797bc585c67ba372d
hato-poppo <ysng1224@gmail.com>
2021-07-02 23:14:23 +0000

    [Change] Site theme.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,9 +9,10 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: colors.lightBlue.lighten1,
-        secondary: colors.lightBlue.base,
-        accent: colors.grey.darken1,
+        primary: colors.brown.base,
+        secondary: colors.orange.accent2,
+        accent: colors.grey.base,
+        transparent: colors.shades.transparent, // この指定で透明にしたかったけど効かず
       },
     },
   },
